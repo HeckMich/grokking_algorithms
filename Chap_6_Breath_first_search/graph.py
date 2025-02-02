6a41c0a25ff2087707b6f47b1e146a7a672c0be2
HeckMich <2310859029@fh-burgenland.at>
2025-02-02 18:31:41 +0100

    feat: adds chapters 6 to 9

---
+++ Chap_6_Breath_first_search/graph.py
@@ -0,0 +1,11 @@
+graph = {}
+graph["Du"] = ["Alice", "Bob", "Claire"]
+graph["Bob"] = ["Anuj", "Peggy"]
+graph["Alice"] = ["Peggy"]
+graph["Claire"] = ["Tom", "Jonny"]
+graph["Anuj"] = []
+graph["Peggy"] = []
+graph["Tom"] = []
+graph["Jonny"] = []
+
+print(graph)
